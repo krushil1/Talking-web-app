11d0ebf57e37a7a2f7724153d080b06b1d809d73
Krushil Amrutiya <krushilamrutiya@gmail.com>
2021-12-21 14:01:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ io.on('connection', socket => {
         socket.join(user.room);
 
         // Welcome current user
-        socket.emit('message', formatMessage(botName, 'Welcome to Vibrr!'));
+        socket.emit('message', formatMessage(botName, 'Welcome to Krushkord!'));
 
         // Broadcast when a user connects
         socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
